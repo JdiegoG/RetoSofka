bfcc2b30d5b838401d7272b1dbb82f974381ba7b
Juan Diego <jdgm97@gmail.com>
2021-07-08 16:56:46 -0500

    [Feature] Firts commit Adding the schema of the game

--- src/App.js
+++ src/App.js
@@ -3,25 +3,62 @@ import './App.css';
 import Game from './Core/Game';
 
 const App = () => {
-  const [isStarted, setIsStarted] = useState(false);
+  const [isStarted, setIsStarted] = useState("PRE-START");
   const [playersNumber, setPlayersNumber] = useState(0);
   const [speedwaylength, setSpeedwaylength] = useState(0);
+  const [currentTurn, setCurrentTurn] = useState(0);
+  const [game, setGame] = useState(new Game());
 
-  const game = new Game();
-
-  const startGame = () => {
+  const startGame = (e) => {
+    e.preventDefault()
     game.startgame(playersNumber, speedwaylength);
-    console.log(game);
-    debugger
+    setIsStarted("DRIVER-NAMES")
   }
 
+  const moverse = ()=>{
+    const lanzamiento = game.carList[currentTurn].lanzardado()
+    game.carList[currentTurn].moverse(lanzamiento, game)
+    const isfinished = game.carList.some(item => item.currentposition < game.speedwaylength)
+    if(!isfinished){
+      setIsStarted("FINISH")
+    }
+    setGame({...new Game(game)})
+    if(currentTurn < (game.carList.length - 1)){
+      setCurrentTurn(currentTurn + 1)
+    }else{
+      setCurrentTurn(0)
+    }
+  }
 
-  return (
-    <div className="App">
-      {
-        isStarted 
-        ? (
-          <form>
+  const deberialanzar = () =>{
+    if(!game.carList[currentTurn].isFinish){
+      return (
+        <div>
+          <h3>{`Turno carrito numero ${currentTurn + 1}`}</h3>
+          <button onClick={moverse}>lanzar Dado</button>
+        </div>
+      )
+    }
+    else{
+      if(currentTurn < (game.carList.length - 1)){
+        setCurrentTurn(currentTurn + 1)
+      }else{
+        setCurrentTurn(0)
+      }
+    }
+  }
+
+  const getCurrentPage = () =>{
+    switch (isStarted) {
+      case "PRE-START":
+        return (
+          <button onClick={() => setIsStarted("START")}>
+            start
+          </button>
+      )
+      case "START":
+        return (
+          <div>
             <label>Ingrese la cantidad de jugadores</label>
             <input type={'number'} value={playersNumber} onChange={(e)=> setPlayersNumber(e.target.value)}></input>
             <br></br>
@@ -29,14 +66,68 @@ const App = () => {
             <input type={'number'} value={speedwaylength} onChange={(e)=> setSpeedwaylength(e.target.value)}></input>
             <br></br>
             <button onClick={startGame}>Iniciar</button>
-          </form>
-        )
-        : (
-          <button onClick={() => setIsStarted(true)}>
-            start
-          </button>
-        )
-      }
+          </div>
+      )
+      case "DRIVER-NAMES":
+        return (
+          <div>
+            {game.carList.map((item,i)=>(
+              <div key={i}>
+                <label>{`Ingresa el nombre del conductor ${i + 1}`}</label>
+                <input value={game.carList[i].driverName} 
+                onChange={(e)=> {
+                  console.log(e.target.value)
+                  game.carList[i].driverName = e.target.value;
+                  setGame({...game})
+                }}></input>
+              </div>
+            ))}
+            <button onClick={()=> setIsStarted("IN-GAME")}>a jugar</button>
+          </div>
+      )
+      case "IN-GAME":
+        console.log(game)
+        return (
+          <div>
+            <h1>Listado de jugadores</h1>
+            <div>
+              {
+                game.carList.map((car, i) =>{
+                  console.log(car)
+                  return (
+                  <div key={i}>
+                    <span>{`Carril numero ${i + 1} `}</span>
+                    <span>{`carrito de ${car.driverName}`}</span>
+                    <span>{`posición en la pista ${car.currentposition * 1000} KM`}</span>
+                  </div>
+                )})
+              }
+            </div>
+            {deberialanzar()}
+          </div>
+      )
+
+      case "FINISH":
+        return (
+          <div>
+            <h1>se acabo el juego</h1>
+            <span>{`primero ${game.podium[0].driverName}`}</span>
+            <br></br>
+            <span>{`segundo ${game.podium[1].driverName}`}</span>
+            <br></br>
+            <span>{`tercero ${game.podium[2].driverName}`}</span>
+          </div>
+      )
+    
+      default:
+        break;
+    }
+  }
+
+
+  return (
+    <div className="App">
+      {getCurrentPage()}
     </div>
   );
 }
--- src/Core/Car.js
+++ src/Core/Car.js
@@ -4,7 +4,22 @@ import Driver from './Driver';
 export default class Car {
     constructor(data) {
         this.id = (data && data.id) || uuidv4();
-        this.driver = (data && data.driver) || new Driver();
+        this.driverName = (data && data.driver) || "";
         this.currentposition = (data && data.currentposition) || 0;
+        this.isFinish = (data && data.isFinish) || false;
+    }
+
+    lanzardado(){
+        const dado6caras = parseInt(6 * Math.random() + 1 );
+        return dado6caras;
+    }
+
+    moverse(lanzamiento, game) {
+        this.currentposition = this.currentposition + lanzamiento
+        if(this.currentposition > game.speedwaylength){
+            this.isFinish = true
+            this.currentposition = game.speedwaylength
+            game.podium.push(this)
+        } 
     }
 }
